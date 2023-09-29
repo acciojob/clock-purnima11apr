5ef41060bdcb2e25c75df96795e4b9e7358a5faf
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-29 14:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ // Function to update the timer
+        function updateTimer() {
+            const timerElement = document.getElementById('timer');
+            const currentDate = new Date();
+            const formattedDate = currentDate.toLocaleDateString();
+            const formattedTime = currentDate.toLocaleTimeString();
+            timerElement.textContent = `${formattedDate} ${formattedTime}`;
+        }
+
+        // Update the timer immediately and then every second
+        updateTimer();
+        setInterval(updateTimer, 1000);
